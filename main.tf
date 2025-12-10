7591fab282011581a088f1f1fe7075e8e28f6675
Radhika884 <radhikaboldiya84@gmail.com>
2025-12-10 14:55:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "example" {
+  name        = "example-sg"
+  description = "Security group allowing SSH and HTTP inbound, all outbound"
+  vpc_id      = "vpc-0b82c352eccef35fd" # <-- Replace this with your actual VPC ID
+
+  # Inbound rules
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rule (allow all)
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "example-sg"
+  }
+}
